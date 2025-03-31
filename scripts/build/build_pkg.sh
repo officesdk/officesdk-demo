941334e231b01b35588a81ff0427a5a3278be1ac
sivenDev <zhengxiwan@163.com>
2025-03-31 17:19:18 +0800

    update release

--- scripts/build/build_pkg.sh
+++ scripts/build/build_pkg.sh
@@ -1,4 +1,3 @@
-
 set -e
 
 export CGO_ENABLED=0
@@ -7,9 +6,21 @@ export GOARCH=$TARGETARCH
 #export GOARCH=amd64
 export APP=office-demo
 
-apk add --no-cach bash make git && make build
+# Detect system type and install dependencies
+if [ -f /etc/alpine-release ]; then
+    # Alpine Linux
+    apk add --no-cache bash make git
+elif [ -f /etc/debian_version ]; then
+    # Debian/Ubuntu
+    apt-get update && apt-get install -y bash make git
+else
+    echo "Unsupported system type"
+    exit 1
+fi
 
-mkdir ./data/ && mkdir ./data/logs/ && mkdir ./data/files/ && mkdir ./data/leveldb/ && mkdir ./data/resource && mkdir ./data/config
+make build
+
+mkdir -p ./data/logs/ ./data/files/ ./data/leveldb/ ./data/resource ./data/config
 mv ./bin/*  ./data/ && cp -r ./config/ ./data/config/ && cp -r ./resource ./data/resource
 
 echo "./${APP} server" >> ./data/start.sh
