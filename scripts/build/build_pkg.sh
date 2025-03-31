dee47c7463a68938e30a4d7f097abcedbe01d418
sivenDev <zhengxiwan@163.com>
2025-03-31 14:42:29 +0800

    update build

--- scripts/build/build_pkg.sh
+++ scripts/build/build_pkg.sh
@@ -12,4 +12,4 @@ apk add --no-cach bash make git && make build
 mkdir ./data/ && mkdir ./data/logs/ && mkdir ./data/files/ && mkdir ./data/leveldb/ && mkdir ./data/resource && mkdir ./data/config
 mv ./bin/*  ./data/ && cp -r ./config/ ./data/config/ && cp -r ./resource ./data/resource
 
-echo "EGO_CONFIG_PATH=${EGO_CONFIG_PATH:-"./config/default.toml"} ./${APP} server" >> ./data/start.sh
+echo "./${APP} server" >> ./data/start.sh
